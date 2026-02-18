1869fc1cab04d35e1aebba5a6f06e5650ddba569
LUCAS RAMOS <akroelfn@gmail.com>
2026-02-18 17:31:55 -0300

    docs: adiciona conteúdo de estrutura
    * feat: adiciona estrutura de programação
    * feat: adiciona estrutura de controle

--- js/app.js
+++ js/app.js
@@ -82,3 +82,36 @@ console.log("Hello from app.js!");
                 console.log(!true)
                 console.log(!5 < 2)
 
+// Estrutura de programação
+    // funções de prompt
+        //const age = prompt("digite sua idade: ");
+        //console.log(`você tem: ${age} anos`)
+
+    // alert
+        // alert("Alerta")
+    
+    // Math 
+    console.log(Math.max(1, 2, 6, 8, 0));
+    console.log(Math.min(1, 2, 6, -2));
+    console.log(Math.ceil(3.14));
+    console.log(Math.floor(3.14));
+    console.log(Math.random());
+
+    // Console
+    console.warn("aviso de atenção!");
+    console.error("error !!!");
+
+// Estrutura de controle
+    // if / else / else if 
+    const salario = 1855;
+    if (salario < 2000) {
+        console.log("seu salario deve ser aumentado!");
+    }
+
+    if (salario > 1900) {
+        console.log("seu salario é mt alto !");
+    } else {
+        console.log("seu salario é mt baixo !");
+    }
+
+
