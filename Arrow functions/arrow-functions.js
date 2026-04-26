b7a0906e16cbb486a2dabf7c7d334ea9e31f4385
LUCAS RAMOS <akroelfn@gmail.com>
2026-04-26 11:23:25 -0300

    Enhance arrow-functions.js with explanations and examples
    Added explanations and examples for arrow functions.

--- Arrow functions/arrow-functions.js
+++ Arrow functions/arrow-functions.js
@@ -1,10 +1,31 @@
 // arrow functions (funções de seta)
+ // são uma forma mais concisa de escrever funções em JavaScript, introduzidas no ES6. Elas são especialmente úteis para funções anônimas e callbacks.
+
+    // 🎯 Pra que usar na prática?
+        // 1. Código mais limpo
+        [1, 2, 3].map(n => n * 2);
+
+        // 2. Funções pequenas (callbacks)
+        setTimeout(() => {
+        console.log("Executou!");
+        }, 1000);
+
+        // 3. NÃO cria seu próprio this
+        function Pessoa() {
+          this.nome = "Lucas";
+
+        setTimeout(() => {
+          console.log(this.nome);
+         }, 1000);
+        }
+    
+    // Exemplo de uso de arrow function
     const arrowFunctionTest = () => {
         console.log("Esta é uma arrow function!");
-    }
+    };
     arrowFunctionTest();
 
-   const ParOuImpar = (numero) => {
+    const ParOuImpar = (numero) => {
         if (numero % 2 === 0) {
             console.log(`O número ${numero} é par.`);
             return;
